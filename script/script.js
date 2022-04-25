87b4078628d45b021de124c20d835ad79d04c13c
pamelarocha <gabrielpr.martins@gmail.com>
2022-04-25 20:17:43 -0300

    fixed form

--- script/script.js
+++ script/script.js
@@ -38,3 +38,58 @@ function handleClickButtonMenu() {
 }
 
 buttonMenu.addEventListener('click', handleClickButtonMenu);
+
+// Form
+const form = document.querySelector('.mei-form');
+const buttonForm = document.querySelector('.button-payment');
+const checkModal = document.querySelector('.check-modal');
+const errorModal = document.querySelector('.error-modal');
+const modalContainer = document.querySelector('.modal-container');
+const buttonCloseModal = document.querySelector('.close-modal');
+
+function resetForm() {
+  const inputs = document.querySelectorAll('.mei-form input');
+  const selects = document.querySelectorAll('.mei-form select');
+  inputs.forEach((input) => (input.value = ''));
+  selects.forEach((select) => (select.value = ''));
+}
+
+function handleOutsideModalClick({ target }) {
+  if (target === modalContainer || target === buttonCloseModal) {
+    resetForm();
+    buttonForm.disabled = false;
+    buttonForm.innerText = 'Enviar';
+    modalContainer.classList.remove(activeClass);
+    checkModal.classList.remove(activeClass);
+    errorModal.classList.remove(activeClass);
+    setTimeout(() =>
+      html.removeEventListener('click', handleOutsideModalClick),
+    );
+  }
+}
+
+function formSent(response) {
+  modalContainer.classList.add(activeClass);
+  if (response.ok) {
+    checkModal.classList.add(activeClass);
+    setTimeout(() => html.addEventListener('click', handleOutsideModalClick));
+  } else {
+    errorModal.classList.add(activeClass);
+    setTimeout(() => html.addEventListener('click', handleOutsideModalClick));
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  buttonForm.disabled = true;
+  buttonForm.innerText = 'Enviando...';
+
+  const data = new FormData(form);
+  console.log(data.get('nome'));
+  fetch('./enviar.php', { method: 'POST', body: data }).then(formSent);
+}
+
+if (form) {
+  form.addEventListener('submit', handleSubmit);
+}
